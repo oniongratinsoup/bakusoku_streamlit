6732cbd3da7f2dc69683a8d7825baa08ac22894d
oniongratinsoup <105699672+oniongratinsoup@users.noreply.github.com>
2022-05-24 15:23:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ expander2.write("問い合わせ2の回答")
 expander3 = st.expander("問い合わせ3")
 expander3.write("問い合わせ3の回答")
 
- text = st.text_input("あなたの趣味を教えて下さい。")
- "あなたの趣味：", text
+text = st.text_input("あなたの趣味を教えて下さい。")
+"あなたの趣味：", text
 
- condition = st.slider("あなたの今の調子は？", 0, 100, 50)
- "コンディション：", condition
+condition = st.slider("あなたの今の調子は？", 0, 100, 50)
+"コンディション：", condition
 
 if st.checkbox("Show Image"):
     img = Image.open("sample_5_17_car.jpg")
