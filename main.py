f8fe21f111ab28ab490fdc5944917fdfcbdc58a0
oniongratinsoup <105699672+oniongratinsoup@users.noreply.github.com>
2022-05-24 15:22:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ bar = st.progress(0)
 for i in range(100):
     latest_iteration.text(f"Iteration {i+1}")
     bar.progress(i + 1)
-    time.sleep(0.1)
-
-    st.balloons()  
+    time.sleep(0.01)
     
 "Done!!!!!"
 
@@ -35,18 +33,18 @@ expander2.write("問い合わせ2の回答")
 expander3 = st.expander("問い合わせ3")
 expander3.write("問い合わせ3の回答")
 
-# text = st.text_input("あなたの趣味を教えて下さい。")
-# "あなたの趣味：", text
+ text = st.text_input("あなたの趣味を教えて下さい。")
+ "あなたの趣味：", text
 
-# condition = st.slider("あなたの今の調子は？", 0, 100, 50)
-# "コンディション：", condition
+ condition = st.slider("あなたの今の調子は？", 0, 100, 50)
+ "コンディション：", condition
 
-#if st.checkbox("Show Image"):
-#    img = Image.open("sample_5_17_car.jpg")
-#    st.image(img, caption="ローレル", use_column_width=True)
+if st.checkbox("Show Image"):
+    img = Image.open("sample_5_17_car.jpg")
+    st.image(img, caption="ローレル", use_column_width=True)
 
-#df = pd.DataFrame(
-#    np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
-#    columns=['lat', 'lon']
-#)
-#st.map(df)
+df = pd.DataFrame(
+    np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
+    columns=['lat', 'lon']
+)
+st.map(df)
